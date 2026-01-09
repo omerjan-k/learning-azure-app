0fd12f5c32680ca74ddf89a8cc12a2682914133d
omerjan-k <omerjan.k@gmail.com>
2026-01-09 15:05:20 +0100

    Add changes after new starten

--- Learningapp/obj/Debug/net10.0/Learningapp.AssemblyInfo.cs
+++ Learningapp/obj/Debug/net10.0/Learningapp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Learningapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eab0f1e0e6b59d8a0b67d70adce3a287e29d0988")]
 [assembly: System.Reflection.AssemblyProductAttribute("Learningapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Learningapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
